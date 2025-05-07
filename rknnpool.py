b2779dec58b8177caf347cade183cabedb024c9b
juanx65 <juan.aguileraca@sansano.usm.cl>
2025-05-07 17:56:33 -0400

    update on multithread

--- rknnpool.py
+++ rknnpool.py
@@ -1,59 +1,75 @@
 from queue import Queue
+from threading import Thread
 from rknnlite.api import RKNNLite
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def initRKNN(rknnModel="./rknnModel/yolov5s.rknn", id=0):
+def initRKNN(rknnModel="./rknnModel/yolov5s.rknn", core_id=0):
     rknn_lite = RKNNLite()
     ret = rknn_lite.load_rknn(rknnModel)
     if ret != 0:
         print("Load RKNN rknnModel failed")
         exit(ret)
-    if id == 0:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0)
-    elif id == 1:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_1)
-    elif id == 2:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_2)
-    elif id == -1:
-        ret = rknn_lite.init_runtime(core_mask=RKNNLite.NPU_CORE_0_1_2)
-    else:
-        ret = rknn_lite.init_runtime()
+
+    core_masks = [RKNNLite.NPU_CORE_0, RKNNLite.NPU_CORE_1, RKNNLite.NPU_CORE_2]
+    core_mask = core_masks[core_id % 3]
+
+    ret = rknn_lite.init_runtime(core_mask=core_mask)
     if ret != 0:
         print("Init runtime environment failed")
         exit(ret)
-    print(rknnModel, "\t\tdone")
+
+    print(f"{rknnModel} initialized on core {core_id}")
     return rknn_lite
 
 
-def initRKNNs(rknnModel="./rknnModel/yolov5s.rknn", TPEs=1):
-    rknn_list = []
-    for i in range(TPEs):
-        rknn_list.append(initRKNN(rknnModel, i % 3))
-    return rknn_list
-
-
-class rknnPoolExecutor():
-    def __init__(self, rknnModel, TPEs, func):
-        self.TPEs = TPEs
-        self.queue = Queue()
-        self.rknnPool = initRKNNs(rknnModel, TPEs)
-        self.pool = ThreadPoolExecutor(max_workers=TPEs)
+class RKNNWorker(Thread):
+    def __init__(self, rknn_index, func, input_queue, output_queue, rknnPool):
+        super().__init__()
+        self.rknn_index = rknn_index
         self.func = func
-        self.num = 0
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.rknnPool = rknnPool
+        self._stop_flag = False
+        self.start()
+
+    def run(self):
+        while True:
+            frame = self.input_queue.get()
+            if frame is None:
+                break
+            result = self.func(self.rknnPool[self.rknn_index], frame)
+            self.output_queue.put(result)
+            self.input_queue.task_done()
+
+
+class rknnPoolExecutor:
+    def __init__(self, rknnModel, TPEs, func):
+        self.TPEs = min(TPEs, 3)
+        self.input_queue = Queue()
+        self.output_queue = Queue()
+        self.rknnPool = [initRKNN(rknnModel, i) for i in range(self.TPEs)]
+        self.workers = [
+            RKNNWorker(i, func, self.input_queue, self.output_queue, self.rknnPool)
+            for i in range(self.TPEs)
+        ]
 
     def put(self, frame):
-        self.queue.put(self.pool.submit(
-            self.func, self.rknnPool[self.num % self.TPEs], frame))
-        self.num += 1
+        self.input_queue.put(frame)
 
     def get(self):
-        if self.queue.empty():
+        try:
+            result = self.output_queue.get(timeout=1)  # espera hasta 1 segundo
+            return result, True
+        except:
             return None, False
-        fut = self.queue.get()
-        return fut.result(), True
 
     def release(self):
-        self.pool.shutdown()
-        for rknn_lite in self.rknnPool:
-            rknn_lite.release()
+        for _ in self.workers:
+            self.input_queue.put(None)
+        for w in self.workers:
+            w.join()
+        # ❗️Importante: liberar los RKNN solo después de que todos los hilos terminaron
+        for rknn in self.rknnPool:
+            rknn.release()
+
