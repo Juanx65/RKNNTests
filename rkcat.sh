87181f304c160c6d639d09460ee13405bbe73c11
juanx65 <juan.aguileraca@sansano.usm.cl>
2025-05-08 17:31:10 -0400

    deletions

--- rkcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Mostrando uso actual de la NPU. Presiona Ctrl+C para salir."
-while true; do
-    echo -n "Uso actual de la NPU: "
-    sudo cat /sys/kernel/debug/rknpu/load
-    sleep 1
-done
-
--- yolo_test.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-model.predict("bus.jpg",save=True)
-
